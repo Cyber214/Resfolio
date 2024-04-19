aa2c80ff646414e11b6621677ff82ae9757408fb
Seun Dada <seun.dada777@gmail.com>
2024-04-19 19:06:39 -0400

    create models, rounts, contollers and set up server

--- models/profile.js
+++ models/profile.js
@@ -5,8 +5,50 @@ const Schema = mongoose.Schema
 const profileSchema = new Schema({
   name: String,
   avatar: String,
+  email: String,
+  phoneNumber: number,
+  resumeUrl: String,
+  education: [eduSchema],
+  application: [appSchema],
 }, {
   timestamps: true
+})
+
+const eduSchema = new Schema({
+  name: {type: String, required: true},
+  degree: {
+    type: String,
+    enum: ['G', 'PG', 'PG-13', 'R']
+  },
+  state: String,
+  country: String,
+  completed: Boolean,
+  startYear: {
+    type: Number, 
+    default: function() {
+      return new Date().getFullYear()
+    }
+  },
+})
+
+const appSchema = new Schema({
+  companyName: {type: String, required: true},
+  prosition: String,
+  jobtype: {
+    type: String,
+    enum: ['part time', 'full time', 'temporary', 'contract']
+  },
+  state: String,
+  country: String,
+  mileRange: {
+    type: Number,
+    enum: ['0', '5', '10', '15', '20']
+  },
+  workDays: {
+    type: String,
+    enum: ['weekend only', 'weekdays only']
+  },
+  availaibility
 })
 
 const Profile = mongoose.model('Profile', profileSchema)
